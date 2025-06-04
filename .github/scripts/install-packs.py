4bbd96d8312c80d423e3dd150dfca5a243686242
Mike Fairhurst <michaelrfairhurst@github.com>
2025-06-04 01:46:36 -0700

    Twelfth try fixing CI/CD; install fewer packs per job

--- .github/scripts/install-packs.py
+++ .github/scripts/install-packs.py
@@ -15,9 +15,16 @@ def get_workspace_packs(root):
 
     return packs
 
+def install_pack(pack_path, codeql_executable, mode):
+    # Run `codeql pack install` to install dependencies.
+    command = [codeql_executable, 'pack', 'install', '--allow-prerelease', '--mode', mode, pack_path]
+    print(f'Running `{" ".join(command)}`')
+    subprocess.check_call(command)
+
 parser = argparse.ArgumentParser(description="Install CodeQL library pack dependencies.")
 parser.add_argument('--mode', required=False, choices=['use-lock', 'update', 'verify', 'no-lock'], default="use-lock", help="Installation mode, identical to the `--mode` argument to `codeql pack install`")
 parser.add_argument('--codeql', required=False, default='codeql', help="Path to the `codeql` executable.")
+parser.add_argument('--language', required=False, help="Which language (or 'common') to install pack dependencies for.")
 args = parser.parse_args()
 
 # Find the root of the repo
@@ -25,11 +32,12 @@ root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)
 
 packs = get_workspace_packs(root)
 
-# Find the CodeQL packs in the repo. This can also return packs outside of the repo, if those packs
-# are installed in a sibling directory to the CLI.
-for pack in packs:
-    pack_path = os.path.join(root, pack)
-    # Run `codeql pack install` to install dependencies.
-    command = [args.codeql, 'pack', 'install', '--allow-prerelease', '--mode', args.mode, pack_path]
-    print(f'Running `{" ".join(command)}`')
-    subprocess.check_call(command)
+# Always install the qtil source pack, which is used by all languages.
+install_pack(os.path.join(root, 'src'), args.codeql, args.mode)
+if args.language == 'common':
+  # common means we want to run test/.
+  install_pack(os.path.join(root, 'test'), args.codeql, args.mode)
+else:
+  # Otherwise, we need to install the language-specific src and test packs.
+  install_pack(os.path.join(root, args.language, 'src'), args.codeql, args.mode)
+  install_pack(os.path.join(root, args.language, 'test'), args.codeql, args.mode)
